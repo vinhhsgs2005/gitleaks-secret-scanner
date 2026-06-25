91cd785a881aa095618c0ee15d6feb6c33a463fc
vinhhsgs2005 <vinhsusu2005@gmail.com>
2026-06-25 18:00:47 +0900

    Update config.py
    add AWS_ACCESS_KEY_ID

--- config.py
+++ config.py
@@ -1,7 +1,4 @@
 # Safe version: no hardcoded secret is stored in the source code.
 # The value should come from an environment variable or GitHub Secret.
 
-import os
-
-AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7EXAMPLE"
